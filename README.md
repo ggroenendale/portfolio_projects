96c9ba60beaecf57af1d46468d3c080938bcf0bb
Geoff Groenendale <g.groenendale@wsu.edu>
2019-02-08 17:37:42 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portfolio Projects
+
+The purpose of this project is to create a WordPress plugin that can handle some of the Project Management and display requirements of a portfolio.
+
+The plugin will allow the admin to keep track of the various requirements, scope, and schedule of a project. It will also be helpful for resource management. This will all be controlled as part of the backend in WordPress.
+
+The plugin will also serve a lot of this information to the frontend as part of the portfolio to display work, current project status, and various updates.
---
+++ port_projects.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ *
+ * @link              http://github.com/ggroenendale/port_gallery
+ * @since             1.0.0
+ * @package           Port_Gallery
+ *
+ *
+ * @wordpress-plugin
+ * Plugin Name: GSquared Concepts Gallery Plugin
+ * Plugin URI: https://github.com/ggroenendale/portgallery
+ * Description: This plugin handles all of the galleries in the portfolio by use of custom post types
+ * Version: 1.0.0
+ * Author: Geoff Groenendale
+ * Author URI: http://www.gsquaredconcepts.com/
+ * License: GPL2
+ * Text-Domain: port_gallery
+ */
